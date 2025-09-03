712cb1a838cf673c40846de81b445937eb97fb4d
matheasborsos <matheasborsos@zaheentelecom.com>
2025-09-03 13:43:12 +0300

    fixed 404 mistake

--- src/components/stripe-payment/StripePayment.jsx
+++ src/components/stripe-payment/StripePayment.jsx
@@ -111,7 +111,7 @@ const InjectedCheckout = ({ orderDetail }) => {
               }
 
               navigate({
-                pathname: iccid ? `/esim/${iccid}` : "/plans",
+                pathname: iccid ? `/esim/${iccid}` : "/plans/land",
                 search: !iccid ? `?${searchParams.toString()}` : "",
               });
             }, 5000); // 5000 ms = 5 seconds
@@ -152,7 +152,7 @@ const InjectedCheckout = ({ orderDetail }) => {
             sx={{ width: "60%" }}
             onClick={() => {
               dispatch(LimitedSignOut());
-              navigate("/plans");
+              navigate("/plans/land");
             }}
           >
             {t("btn.cancel")}
